7554ab6b6ef7dfad63ffe4cd9e38c7ea3b65b8e3
limmi26 <kisamakiazuel@gmail.com>
2022-03-01 11:38:02 +0700

    perubahan 3

--- appleTree.js
+++ appleTree.js
@@ -2,18 +2,23 @@
 
 class AppleTree {
     // constructor
-    constructor(treeLimitAge,treeMatrueAge,treeStopAge) {
-        this._age = 0;
-        this._height = 0;
+    constructor(tree_name,tree_age,tree_height,treeMatrueAge,treeHealthStatus,treeLimitAge,treeStopAge) {
+        this._name = tree_name;
+        this._age = tree_age;
+        this._height = tree_height;
         this._fruits = [];
         this.harvested = '';
-        this._healthStatus = true;
+        this._healthStatus = treeHealthStatus;
+        this._MatrueAge = treeMatrueAge;;
         this._limitAge = treeLimitAge;
-        this._matrueAge = treeMatrueAge;
         this._stopAge = treeStopAge;
     }
 
     // getter
+    get name() {
+        return this._name;
+    }
+
     get age() {
         return this._age; 
     }
@@ -34,16 +39,12 @@ class AppleTree {
         return this._harvested;
     }
 
-    get limitAge() {
-        return this._limitAge;
-    }
-    
     get matureAge() {
         return this._matureAge;
     }
 
-    get stopAge() {
-        return this._stopAge;
+    get limitAge() {
+        return this._limitAge;
     }
 
     // setter
@@ -51,38 +52,32 @@ class AppleTree {
         this._age = value;
     }
 
-    // growing process
+    // grow
     grow() {
-        this.age++;
-        let addingHeigh = 0;
-        
-        if(this.age < this.limitAge){
-            addingHeigh = math.round(math.random*100)/100;
-            this.height = addingHeigh;
+        this._age++;
+        /*
+         * buatlah sebuah kondisi jika kalau umur pohon lebih besar atau sama dengan dari limit umur limit-nya maka pohon tidak sehat
+         * jika umur pohon lebih kecil dari umur pohon berhenti tumbuh, maka pohon bertambah tinggi secara random
+         * tinggi pohon dibulatkan ke bulatan terbesar
+        */
+        if (this._age <= this._limitAge ) {
+            const pertumbuhan = Math.ceil(Math.random() * this._height);
+            return this._height + pertumbuhan;
+        } else {
+            console.log('pohon ini sudah mati');
         }
-
-        return this.age;
     }
 
-    // produce fruits
+    // produce some fruits
     produceFruits() {
-        if(this.age >= this.matureAge) {
-            // 1. total buah
-            let fruitsTotal = math.round(math.random*100);
-            for(let i; i <= fruitsTotal; 1++) {
-                this.fruits.push(i);
-            }
-            // 2. bagus/jelek - kualitas buah
-        }
-
         /* 
           * jika pohon ini sudah mencapai umur yang matang (mature) atau lebih besar dari umur matangnya, maka pohon akan berbuah 
           * buat kelas buah di luar kelas pohon
           * hasil buah akan disimpan di dalam seperti fruit
         */
     }
-
-    // harvesting/panen
+    
+    // get some fruit
     harvest() {
         /*
          * menentukan jumlah seluruh buah
@@ -90,8 +85,6 @@ class AppleTree {
     }
 }
 
-class Fruits {
-    constructor(num) {
-        this.quanty = 
-    }
-}
+const pohonMangga = new AppleTree('pohon apel', 30, 150, true, 50, 100, 100);
+
+console.log(pohonMangga.grow());
